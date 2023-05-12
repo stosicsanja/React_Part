9d4167861c44a552c603890249410c54f46def5e
DESKTOP-EF2VHJM\ivana <dobrevolje1@gmail.com>
2023-05-12 19:37:42 +0200

    changed photo

--- my-app/src/header/Header.jsx
+++ my-app/src/header/Header.jsx
@@ -6,10 +6,7 @@ function Header() {
     <div className="headerTitles">
         <div className="headerTitleLg">BLOG</div>
     </div>
-    <img className="headerImg"
-    alt=""
-    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ZxZkb5j96PWhXTCnDCij8jD64t0ipj1AQU1rV-c6jHFP_vb7Q_FUQzkL3xGcVpLX0oQ&usqp=CAU"
-    ></img>
+  
     </div>
   )
 }
